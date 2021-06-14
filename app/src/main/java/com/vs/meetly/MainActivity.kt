b9b28b3319d49eb5aaa38899f604fbc212f01916
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-14 23:12:17 +0530

    Meetly :iphone:

---
+++ app/src/main/java/com/vs/meetly/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vs.meetly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //start with splash screen using postDelayedMethod
+
+    }
+}
