e3210d17bbab4c4c2cb59d748dd1f93936e81a3e
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-15 08:43:28 +0530

    Changes
    1. Login Activity
    2. Register Activity
    3. Splash Screen
    4. Connect firebase
    5. Register user to firebase using email  and password auth
    
    @Xfinity-bot
    
    Co-Authored-By: Xfinity <65854432+Xfinity-bot@users.noreply.github.com>

---
+++ app/src/main/java/com/vs/meetly/RegisterActivity.kt
@@ -0,0 +1,38 @@
+package com.vs.meetly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        buttonRegister.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser(){
+        val email = etvEmail.text.toString()
+        val password = etvPassword.text.toString()
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful){
+                    Toast.makeText(this, "Registration Successfull!", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Error, while creating user!", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
