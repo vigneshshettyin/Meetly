a5345ae3135833dd3602394058692b69d9f4274c
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-15 11:33:56 +0530

    LoginActivity.kt Updated!
    1. Login & Register Activity Tested
     Status: Success!

--- app/src/main/java/com/vs/meetly/LoginActivity.kt
+++ app/src/main/java/com/vs/meetly/LoginActivity.kt
@@ -2,10 +2,38 @@ package com.vs.meetly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        etvLogin.setOnClickListener {
+            login()
+        }
+
+    }
+
+    private fun login() {
+        val email = etvEmail.text.toString()
+        val password =etvPassword.text.toString()
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful){
+                    Toast.makeText(this, "Login Successfull!", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Incorrect login credentials or account not found!", Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 }
