62c305adf86d1ced17829e29b73dada534ce6b7c
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-16 17:00:50 +0530

    Drawer Menu Layout
    1. Side Bar Added
    2. Linked With Data From Firestore
    3. Menu Layout & Menu Icon Set
    4. Menu Bar Items On Click To Be Handled.
    
    @Xfinity-bot

--- app/src/main/java/com/vs/meetly/MainActivity.kt
+++ app/src/main/java/com/vs/meetly/MainActivity.kt
@@ -2,12 +2,15 @@ package com.vs.meetly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.vs.meetly.daos.UserDao
 import com.vs.meetly.modals.User
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.header_layout.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -15,6 +18,8 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     private lateinit var auth : FirebaseAuth
 
@@ -24,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpViews()
+
         auth = FirebaseAuth.getInstance()
         testProgress.visibility = View.VISIBLE
 
@@ -32,15 +39,33 @@ class MainActivity : AppCompatActivity() {
                 val userDao = UserDao()
                 user = userDao.getUserById(currentUserId).await().toObject(User::class.java)!!
                 withContext(Dispatchers.Main){
-                    demoTest.text = user.displayName
-                    demoTest2.text  = user.uid
+                    header_title.text = user.displayName
+                    user_id.text  = user.uid
                     loadImage(user.imageUrl)
                 }
             }
     }
 
     private fun loadImage(imageUrl: String) {
-        Glide.with(this).load(imageUrl).into(image)
+        Glide.with(this).load(imageUrl).into(header_image)
         testProgress.visibility = View.GONE
     }
+
+    private fun setUpViews() {
+        setUpDrawerLayout()
+    }
+
+    private fun setUpDrawerLayout() {
+        setSupportActionBar(appBar)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, mainDrawer, R.string.app_name, R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
