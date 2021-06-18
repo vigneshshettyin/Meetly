f6d86169cbf1a2af2f41aab1dbc6f98e35c404e4
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-18 22:00:33 +0530

    Delete Option Implemented
    Delete Option implemented -> Data Will  Be Deleted From Local List Also From Firestore The Main NOSQL Database

--- app/src/main/java/com/vs/meetly/MeetingFilter.kt
+++ app/src/main/java/com/vs/meetly/MeetingFilter.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.firestore.FirebaseFirestore
+import com.vs.meetly.adapters.IMeetingRVAdapter
 import com.vs.meetly.adapters.MeetingAdapter
 import com.vs.meetly.modals.Meeting
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_meeting_filter.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 
-class MeetingFilter : AppCompatActivity() {
+class MeetingFilter : AppCompatActivity(), IMeetingRVAdapter {
 
     lateinit var adapter: MeetingAdapter
 
@@ -49,9 +53,52 @@ class MeetingFilter : AppCompatActivity() {
     }
 
     private fun setUpRecyclerView() {
-        adapter = MeetingAdapter(this, meetingList)
+        adapter = MeetingAdapter(this, meetingList, this)
         meetingFilterRecyclerView.layoutManager = LinearLayoutManager(this)
         meetingFilterRecyclerView.adapter = adapter
+    }
+
+    override fun onItemClicked(meeting: Meeting) {
+//        Toast.makeText(this, meeting.content, Toast.LENGTH_SHORT).show()
+        val meetingColRef = firestore.collection("meetings")
+        CoroutineScope(Dispatchers.IO).launch {
+            val meetingQuery = meetingColRef
+                .whereEqualTo("content", meeting.content)
+                .whereEqualTo("date", meeting.date)
+                .whereEqualTo("time", meeting.time)
+                .get()
+                .await()
+            if(meetingQuery.documents.isNotEmpty()) {
+                for(document in meetingQuery) {
+                    try {
+                        withContext(Dispatchers.Main) {
+                            MaterialAlertDialogBuilder(
+                                this@MeetingFilter,
+                                R.style.Base_ThemeOverlay_MaterialComponents_MaterialAlertDialog
+                            )
+                                .setMessage(resources.getString(R.string.confirm_logout))
+                                .setNegativeButton(resources.getString(R.string.cancel)) { dialog, which ->
+                                    // Respond to negative button press
+                                }
+                                .setPositiveButton(resources.getString(R.string.yes)) { dialog, which ->
+                                    GlobalScope.launch {
+                                        meetingColRef.document(document.id).delete().await()
+                                    }
+                                }
+                                .show()
+                        }
+                    } catch (e: Exception) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@MeetingFilter, e.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MeetingFilter, "No meetings matched the query.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
 }
