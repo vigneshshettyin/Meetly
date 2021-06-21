3907c34a5382a284477d7047191af2ea13bd5b0f
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-21 23:04:14 +0530

    Cardview Alert ( ProgressBar )

--- app/src/main/java/com/vs/meetly/SplashActivity.kt
+++ app/src/main/java/com/vs/meetly/SplashActivity.kt
@@ -1,25 +1,31 @@
+@file:Suppress("DEPRECATION")
+
 package com.vs.meetly
 
-import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import android.view.View
+import android.view.WindowManager
 import androidx.annotation.RequiresApi
 import com.vs.meetly.internetcheck.isNetworkAvailable
 
-@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        hideDefaultUI()
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
 
         Handler().postDelayed({
             val intentl = Intent(this, LoginActivity::class.java)
@@ -31,9 +37,5 @@ class SplashActivity : AppCompatActivity() {
             finish()
         }, 3000)
 
-    }
-
-    private fun hideDefaultUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
