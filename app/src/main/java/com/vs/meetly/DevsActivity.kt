db556984d4a1fb4716e9fd9e8e93c57b6e13f1e0
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-25 11:15:25 +0530

    Bug Fix & Chrome Custom Tabs Integration
    Developer Support Section Bug Fixes.
    
    :ok:

--- app/src/main/java/com/vs/meetly/DevsActivity.kt
+++ app/src/main/java/com/vs/meetly/DevsActivity.kt
@@ -1,32 +1,37 @@
 package com.vs.meetly
 
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import kotlinx.android.synthetic.main.activity_meeting_filter.*
 
 
 class DevsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_devs)
+
+        topAppBar.setNavigationOnClickListener {
+            finish()
+        }
+
+
     }
 
     fun iVclick(view: View) {
         var url=""
         when(view.id){
             R.id.viVgithub->{ url="https://github.com/vigneshshettyin" }
-            R.id.viVgmail->{url="mailto:vigneshshettyalike@gmail.com" }
+            R.id.viVgmail->{url="mailto:vigneshshetty.in@gmail.com" }
             R.id.viVlinkedin->{url="https://www.linkedin.com/in/vigneshshettyin/"}
             R.id.siVgithub->{url="https://github.com/Xfinity-bot" }
             R.id.siVgmail->{url="mailto:sriganesh7334@gmail.com" }
             R.id.siVlinkedin->{url="https://www.linkedin.com/in/sriganesh-rao-1b6a921a5/"}
-
-
         }
-        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(browserIntent)
+        val builder = CustomTabsIntent.Builder()
+        val CustomTabsIntent = builder.build()
+        CustomTabsIntent.launchUrl(this, Uri.parse(url))
     }
 }
