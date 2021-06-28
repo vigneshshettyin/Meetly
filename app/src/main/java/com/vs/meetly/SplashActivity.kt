d431caf06250b6c70636f75cac42bc8669846458
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-28 13:05:59 +0530

    Permission Management!
    Check for active permissions in splash screen itself. To avoid bug in edit project section.

--- app/src/main/java/com/vs/meetly/SplashActivity.kt
+++ app/src/main/java/com/vs/meetly/SplashActivity.kt
@@ -1,16 +1,18 @@
-@file:Suppress("DEPRECATION")
-
 package com.vs.meetly
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import com.vs.meetly.internetcheck.isNetworkAvailable
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -24,6 +26,24 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET),
+                111
+            )
+        } else {
+            moveToNextIntent()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun moveToNextIntent() {
         Handler().postDelayed({
             val intentl = Intent(this, LoginActivity::class.java)
             val intentno = Intent(this, NoInternet::class.java)
@@ -33,6 +53,17 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(intentno)
             finish()
         }, 3000)
+    }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            moveToNextIntent()
+        }
     }
 }
