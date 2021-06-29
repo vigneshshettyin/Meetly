d4cea1d407892590176eba31a64ab0a0b34322ca
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-06-29 14:59:53 +0530

    Cloud Meeting Option!
    Using Jitsi

--- app/src/main/java/com/vs/meetly/MeetingViewDetail.kt
+++ app/src/main/java/com/vs/meetly/MeetingViewDetail.kt
@@ -1,12 +1,18 @@
 package com.vs.meetly
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
@@ -18,10 +24,20 @@ import kotlinx.android.synthetic.main.activity_meeting_view_detail.*
 import kotlinx.android.synthetic.main.dialog_new_user_meeting.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.tasks.await
+import org.jitsi.meet.sdk.*
+import timber.log.Timber
+import java.net.MalformedURLException
+import java.net.URL
 import java.util.*
 import kotlin.collections.ArrayList
 
 class MeetingViewDetail : AppCompatActivity() {
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            onBroadcastReceived(intent)
+        }
+    }
 
     private lateinit var auth: FirebaseAuth
 
@@ -65,6 +81,30 @@ class MeetingViewDetail : AppCompatActivity() {
         add_new_user.setOnClickListener {
             showEditTextDialog()
         }
+
+        // Initialize default options for Jitsi Meet conferences.
+        val serverURL: URL
+        serverURL = try {
+            // When using JaaS, replace "https://meet.jit.si" with the proper serverURL
+            URL("https://meet.jit.si")
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            throw RuntimeException("Invalid server URL!")
+        }
+        val defaultOptions = JitsiMeetConferenceOptions.Builder()
+            .setServerURL(serverURL)
+            // When using JaaS, set the obtained JWT here
+            //.setToken("MyJWT")
+            // Different features flags can be set
+            //.setFeatureFlag("toolbox.enabled", false)
+            //.setFeatureFlag("filmstrip.enabled", false)
+            .setWelcomePageEnabled(false)
+            .build()
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions)
+
+        registerForBroadcastMessages()
+
+
 
     }
 
@@ -159,4 +199,63 @@ class MeetingViewDetail : AppCompatActivity() {
     private fun alertUserNotPresent() {
         SupportClass.alertUserDanger(this, "User don't have a account at Meetly!")
     }
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver)
+        super.onDestroy()
+    }
+
+
+    fun onButtonClick(v: View?) {
+        val text = meeting.meeting_link
+        if (text.length > 0) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            val options = JitsiMeetConferenceOptions.Builder()
+                .setRoom(text)
+                // Settings for audio and video
+                //.setAudioMuted(true)
+                //.setVideoMuted(true)
+                .build()
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options)
+        }
+    }
+
+
+    private fun registerForBroadcastMessages() {
+        val intentFilter = IntentFilter()
+
+        /* This registers for every possible event sent from JitsiMeetSDK
+           If only some of the events are needed, the for loop can be replaced
+           with individual statements:
+           ex:  intentFilter.addAction(BroadcastEvent.Type.AUDIO_MUTED_CHANGED.action);
+                intentFilter.addAction(BroadcastEvent.Type.CONFERENCE_TERMINATED.action);
+                ... other events
+         */
+        for (type in BroadcastEvent.Type.values()) {
+            intentFilter.addAction(type.action)
+        }
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    // Example for handling different JitsiMeetSDK events
+    private fun onBroadcastReceived(intent: Intent?) {
+        if (intent != null) {
+            val event = BroadcastEvent(intent)
+            when (event.getType()) {
+                BroadcastEvent.Type.CONFERENCE_JOINED -> Timber.i("Conference Joined with url%s", event.getData().get("url"))
+                BroadcastEvent.Type.PARTICIPANT_JOINED -> Timber.i("Participant joined%s", event.getData().get("name"))
+            }
+        }
+    }
+
+    // Example for sending actions to JitsiMeetSDK
+    private fun hangUp() {
+        val hangupBroadcastIntent: Intent = BroadcastIntentHelper.buildHangUpIntent()
+        LocalBroadcastManager.getInstance(org.webrtc.ContextUtils.getApplicationContext()).sendBroadcast(hangupBroadcastIntent)
+    }
+
+
 }
