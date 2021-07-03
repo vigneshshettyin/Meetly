13f86170cd843cf0e83ceb60fbea16bc5ee148d7
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-07-03 22:14:14 +0530

    Forgot Password Validations

--- app/src/main/java/com/vs/meetly/ForgotPassActivity.kt
+++ app/src/main/java/com/vs/meetly/ForgotPassActivity.kt
@@ -3,8 +3,10 @@ package com.vs.meetly
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_pass.*
+import kotlinx.android.synthetic.main.activity_forgot_pass.etvEmail
 
 class ForgotPassActivity : AppCompatActivity() {
 
@@ -24,14 +26,31 @@ class ForgotPassActivity : AppCompatActivity() {
 
         val email = etvEmail.text.toString()
 
-        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(this) {
-            if (it.isComplete) {
-                Toast.makeText(this, "Rest Email Generated!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Something went wrong, ${it.toString()}!", Toast.LENGTH_SHORT)
-                    .show()
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z0-9.]+"
+
+        if (email.isEmpty()) {
+            Snackbar.make(
+                ForgotPasswordSnackbar,
+                "Enter all the fields!", Snackbar.LENGTH_LONG
+            )
+                .show()
+        } else if (!email.matches(emailPattern.toRegex())) {
+            Snackbar.make(
+                ForgotPasswordSnackbar,
+                "Enter a valid email id!", Snackbar.LENGTH_LONG
+            )
+                .show()
+        }
+        else{
+            firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(this) {
+                if (it.isComplete) {
+                    Toast.makeText(this, "Rest email generated successfully!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Something went wrong, ${it.toString()}!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                finish()
             }
-            finish()
         }
     }
 }
