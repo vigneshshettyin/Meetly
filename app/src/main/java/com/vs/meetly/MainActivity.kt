10dfd56ae405761c864a4575b57c5ecf649d8d69
Vignesh Shetty <77713888+vigneshshettyin@users.noreply.github.com>
2021-07-08 14:01:20 +0530

    tawk.to
    Raise Request Section Implemented.
    
    http://tawk.to/

--- app/src/main/java/com/vs/meetly/MainActivity.kt
+++ app/src/main/java/com/vs/meetly/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vs.meetly
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,6 +14,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -229,6 +231,14 @@ class MainActivity : AppCompatActivity(), IMeetingRVAdapter {
                     mainDrawer.closeDrawers()
                     val intent = Intent(this, NewMeeting::class.java)
                     startActivity(intent)
+                    true
+                }
+                R.id.raiseRequest ->{
+                    mainDrawer.closeDrawers()
+                    val url = "https://meetly.tawk.help"
+                    val builder = CustomTabsIntent.Builder()
+                    val CustomTabsIntent = builder.build()
+                    CustomTabsIntent.launchUrl(this, Uri.parse(url))
                     true
                 }
                 R.id.devSupport -> {
