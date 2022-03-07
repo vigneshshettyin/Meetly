bab5c9616b7e534a86eb766f7ecf4b0cf1fa1ae3
priyanshuborole <ppborole1762002@gmail.com>
2022-03-07 18:54:59 +0530

    added libraries section

---
+++ app/src/main/java/com/vs/meetly/LibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.vs.meetly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
