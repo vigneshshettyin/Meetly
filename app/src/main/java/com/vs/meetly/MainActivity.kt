d195453d573d91b6c8e714ef650840f8284e2d8c
Debashish Kundu <debashish.joy@gmail.com>
2022-04-01 14:00:35 +0530

    Used new font and implemented curved edges in exit dialog

--- app/src/main/java/com/vs/meetly/MainActivity.kt
+++ app/src/main/java/com/vs/meetly/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -116,17 +117,13 @@ class MainActivity : AppCompatActivity(), IMeetingRVAdapter {
                 .show()
 
         } else {
-            MaterialAlertDialogBuilder(
-                this@MainActivity,
-                R.style.Base_ThemeOverlay_MaterialComponents_MaterialAlertDialog
-            )
-                .setMessage(resources.getString(R.string.exitMessage))
-                .setNegativeButton(resources.getString(R.string.cancel)) { dialog, which ->
-                    // Respond to negative button press
+            MaterialAlertDialogBuilder(this)
+                .setNegativeButton(resources.getString(R.string.cancel)) { _, _ -> }
+                .setPositiveButton(resources.getString(R.string.yes)) { _, _ ->
+                    finishAffinity()
                 }
-                .setPositiveButton(resources.getString(R.string.yes)) { dialog, which ->
-                    super.onBackPressed()
-                }
+                .setBackground(ContextCompat.getDrawable(this, R.drawable.curved_exit_dialog))
+                .setView(layoutInflater.inflate(R.layout.exit_dialog, null))
                 .show()
         }
     }
